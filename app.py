1f3d9cb956ab1ee37ff41cb0442eea9fc476c902
chenjinliang <chen_koma@163.com>
2021-10-14 16:51:27 +0800

    mysql demo

--- app.py
+++ app.py
@@ -25,22 +25,58 @@ def initdb(drop):
 #     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
 @app.route('/')
 def index():
-    name = 'Grey Li'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-    ]
+    # name = 'Grey Li'
+    # movies = [
+    #     {'title': 'My Neighbor Totoro', 'year': '1988'},
+    #     {'title': 'Dead Poets Society', 'year': '1989'},
+    #     {'title': 'A Perfect World', 'year': '1993'},
+    #     {'title': 'Leon', 'year': '1994'},
+    #     {'title': 'Mahjong', 'year': '1996'},
+    #     {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    #     {'title': 'King of Comedy', 'year': '1999'},
+    #     {'title': 'Devils on the Doorstep', 'year': '1999'},
+    #     {'title': 'WALL-E', 'year': '2008'},
+    #     {'title': 'The Pork of Music', 'year': '2012'},
+    # ]
     return render_template('index.html', name=name, movies=movies)
+
+
+@app.route('/db')
+def index_db():
+    user = User.query.first()
+    print(user.name)
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
 
 @app.route('/user/<name>')
